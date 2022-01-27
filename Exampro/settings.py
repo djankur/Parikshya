1c6e197e317396c7af49f938805a799be6f5c091
Ankur <41209837+djankur@users.noreply.github.com>
2022-01-28 01:11:02 +0530

    commit

--- Exampro/settings.py
+++ Exampro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p!ux^f4+=gy)s44e)x2it3m3@$urr=jf&07)m*-9_&$pod_2be
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','parikshya.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','parikshya.herokuapp.com/']
 
 # DEBUG = False
 
